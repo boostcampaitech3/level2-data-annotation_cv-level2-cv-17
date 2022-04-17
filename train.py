e13f286ee1530f6204688f118af7a4fd71e9f5e6
Dongwoo-Im <cksgus159@naver.com>
2022-04-17 18:48:39 +0000

    [refactor] train.py & [fix] userwarning
    - I refactor train.py by adding comment and modify a little code
    - I resolve userwanring by removing png extension

--- train.py
+++ train.py
@@ -1,51 +1,42 @@
 import os
 import os.path as osp
-import time
-import math
-from datetime import timedelta
 from argparse import ArgumentParser
 
 import torch
 from torch import cuda
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
-from tqdm import tqdm
 
-from east_dataset import EASTDataset
-from dataset import SceneTextDataset
-from model import EAST
-
-import gc
-import yaml
-import wandb
-from functools import reduce, partial
-
-import cv2
+import math
 import json
+import yaml
 from glob import glob
+from tqdm import tqdm
+from functools import reduce, partial
+import wandb
 
+# baseline
+from dataset import SceneTextDataset
+from east_dataset import EASTDataset
+from model import EAST
+from inference import do_inference
+from deteval import calc_deteval_metrics
+
+# ours
 from sweep import update_args, get_sweep_cfg
 from utils import increment_path, set_seeds, read_json
 from custom_scheduler import CosineAnnealingWarmUpRestarts
-
-from deteval import calc_deteval_metrics
-from inference import do_inference
-
-import warnings
-warnings.filterwarnings(action='ignore')
-# /opt/conda/lib/python3.8/site-packages/PIL/Image.py:975:
-# UserWarning: Palette images with Transparency expressed in bytes should be converted to RGBA images
 
 
 def parse_args():
     parser = ArgumentParser()
     # directory
     parser.add_argument('--data_dir', type=str,
-                        default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/ICDAR17_Korean'))
+                        default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/ICDAR17_ALL'))
     parser.add_argument('--val_data_dir', type=str,
                         default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/ICDAR17_Korean'))
     parser.add_argument('--json_dir', type=str,
-                        default='/opt/ml/input/data/ICDAR17_Korean/ufo/train.json', help='train json directory')
+                        default='/opt/ml/input/data/ICDAR17_ALL/ufo/train.json', help='train json directory')
     parser.add_argument('--val_json_dir', type=str,
                         default='/opt/ml/input/data/ICDAR17_Korean/ufo/train.json', help='valid json directory')
     parser.add_argument('--work_dir', type=str, default='./work_dirs',
@@ -54,8 +45,8 @@ def parse_args():
     parser.add_argument('--device', type=str, default='cuda' if cuda.is_available() else 'cpu')
     parser.add_argument('--seed', type=int, default=42, help='random seed')
     parser.add_argument('--num_workers', type=int, default=8, help='dataloader num_workers')
-    parser.add_argument('--save_interval', type=int, default=10, help='model save interval')
-    parser.add_argument('--save_max_num', type=int, default=5, help='the max number of model save files')
+    parser.add_argument('--save_interval', type=int, default=5, help='model save interval')
+    parser.add_argument('--save_max_num', type=int, default=10, help='the max number of model save files')
     parser.add_argument('--train_eval', type=bool, default=False, help='boolean about evaluation on train dataset')
     parser.add_argument('--eval_interval', type=int, default=1, help='evaluation metric log interval')
     # training parameter
@@ -82,18 +73,16 @@ def do_training(
     ):
     set_seeds(seed)
     
-    # json_name can controlled by using args.json_dir
+    # you can control json_name by adjusting args.json_dir
     json_name = json_dir.split('/')[-1].split('.')[0]
-    dataset = SceneTextDataset(data_dir, split=json_name,
-                               image_size=image_size, crop_size=input_size)
+    dataset = SceneTextDataset(data_dir, split=json_name, image_size=image_size, crop_size=input_size)
     dataset = EASTDataset(dataset)
     num_batches = math.ceil(len(dataset) / batch_size)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     
-    # val_json_name can controlled by using args.val_json_dir
+    # you can control val_json_name by adjusting args.val_json_dir
     val_json_name = val_json_dir.split('/')[-1].split('.')[0]
-    val_dataset = SceneTextDataset(val_data_dir, split=val_json_name,
-                                   image_size=image_size, crop_size=input_size)
+    val_dataset = SceneTextDataset(val_data_dir, split=val_json_name, image_size=image_size, crop_size=input_size)
     val_dataset = EASTDataset(val_dataset)
     val_num_batches = math.ceil(len(val_dataset) / batch_size)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
@@ -103,6 +92,7 @@ def do_training(
     model.to(device)
 
     # optimizer
+    # if you want to use CosineAnnealingWarmUpRestarts, optimizer must be started at lr=0
     if optm == 'adam':
         if schd == 'cosignlr':
             optimizer = torch.optim.Adam(model.parameters(), lr=0)
@@ -120,7 +110,6 @@ def do_training(
     elif schd == 'reducelr':
         scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
     elif schd == 'cosignlr':
-        # when being used with CosineAnnealingWarmUpRestarts, optimizer must start from lr=0
         scheduler = CosineAnnealingWarmUpRestarts(
             optimizer, T_0=50, T_mult=1, eta_max=learning_rate, T_up=5, gamma=0.5)
     
@@ -145,6 +134,7 @@ def do_training(
                 epoch_iou_loss += extra_info['iou_loss']
 
                 pbar.update(1)
+            # set_postfix is about one epoch
             pbar.set_postfix({
                 'loss': epoch_loss/num_batches, 'cls_loss': epoch_cls_loss/num_batches,
                 'ang_loss': epoch_ang_loss/num_batches, 'iou_loss': epoch_iou_loss/num_batches,
@@ -153,8 +143,8 @@ def do_training(
         # train evaluation
         if train_eval is not False and (epoch + 1) % eval_interval == 0:
             gt_ufo = read_json(json_dir)
-            # ckpt_fpath : already we have model, so we don't use ckpt
-            # split : image folder name
+            # ckpt_fpath : we don't use in here
+            # split : train image_folder_name
             pred_ufo = do_inference(model=model, input_size=input_size, batch_size=batch_size,
                                     data_dir=data_dir, ckpt_fpath=None, split='images')
             
@@ -182,6 +172,7 @@ def do_training(
                 val_epoch_iou_loss += extra_info['iou_loss']
 
                 pbar.update(1)
+            # set_postfix is about one epoch
             pbar.set_postfix({
                 'loss': val_epoch_loss/val_num_batches, 'cls_loss': val_epoch_cls_loss/val_num_batches,
                 'ang_loss': val_epoch_ang_loss/val_num_batches, 'iou_loss': val_epoch_iou_loss/val_num_batches,
@@ -190,8 +181,8 @@ def do_training(
         # valid evaluation
         if (epoch + 1) % eval_interval == 0:
             gt_ufo = read_json(val_json_dir)
-            # ckpt_fpath : already we have model, so we don't use ckpt
-            # split : image folder name
+            # ckpt_fpath : we don't use in here
+            # split : valid image_folder_name
             pred_ufo = do_inference(model=model, input_size=input_size, batch_size=batch_size,
                                     data_dir=val_data_dir, ckpt_fpath=None, split='images')
             val_epoch_precison, val_epoch_recall, val_epoch_hmean = do_evaluating(gt_ufo, pred_ufo)
@@ -207,12 +198,15 @@ def do_training(
         else:
             scheduler.step()
         
+        # save model checkpoint
         if (epoch + 1) % save_interval == 0:
             ckpt_fpath = osp.join(work_dir_exp, f'epoch_{epoch + 1}.pth')
             torch.save(model.state_dict(), ckpt_fpath)
-            if len(glob(osp.join(work_dir_exp,'*.pth'))) > save_max_num:
-                pth_list = [f.split('/')[-1].split('.')[0].split('_')[-1] for f in glob(osp.join(work_dir_exp,'*.pth'))]
-                os.remove(osp.join(work_dir_exp, f'epoch_{sorted(pth_list, key=lambda x: int(x))[0]}.pth'))
+            pth_files = glob(osp.join(work_dir_exp,'*.pth'))
+            if len(pth_files) > save_max_num:
+                epoch_num_list = [f.split('/')[-1].split('.')[0].split('_')[-1] for f in pth_files]
+                min_epoch_num = sorted(epoch_num_list, key=lambda x: int(x))[0]
+                os.remove(osp.join(work_dir_exp, f'epoch_{min_epoch_num}.pth'))
 
         wandb.log({
             "train/loss": epoch_loss/num_batches, "valid/loss": val_epoch_loss/val_num_batches,
@@ -224,25 +218,22 @@ def do_training(
 
 def do_evaluating(gt_ufo, pred_ufo):
     epoch_precison, epoch_recall, epoch_hmean = 0, 0, 0
-    num_images = len(gt_ufo['images'])
-    # it calculate for each image
+    
     for pred_image, gt_image in zip(sorted(pred_ufo['images'].items()), sorted(gt_ufo['images'].items())):
         pred_bboxes_dict, gt_bboxes_dict, gt_trans_dict = {}, {}, {}
         pred_bboxes_list, gt_bboxes_list, gt_trans_list = [], [], []
-
-        # format change
+        
         for pred_point in range(len(pred_image[1]['words'])):
             pred_bboxes_list.extend([pred_image[1]['words'][pred_point]['points']])
         pred_bboxes_dict[pred_image[0]] = pred_bboxes_list
         
-        # format change
         for gt_point in range(len(gt_image[1]['words'])):
             gt_bboxes_list.extend([gt_image[1]['words'][str(gt_point)]['points']])
             gt_trans_list.extend([gt_image[1]['words'][str(gt_point)]['transcription']])
         gt_bboxes_dict[gt_image[0]] = gt_bboxes_list
         gt_trans_dict[gt_image[0]] = gt_trans_list
         
-        # eval_metric['total'] : this consider all of bboxes in each image
+        # eval_metric['total'] : this value is about all of bboxes in one image
         eval_metric = calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict=gt_trans_dict)
         epoch_precison += eval_metric['total']['precision']
         epoch_recall += eval_metric['total']['recall']
@@ -262,6 +253,7 @@ def main(args):
         wandb_run.name = args.work_dir_exp.split('/')[-1]  # run name
         
         args = update_args(args, wandb.config)
+        
         # save args as yaml file every experiment
         yamldir = osp.join(os.getcwd(), args.work_dir_exp+'/train_config.yml')
         with open(yamldir, 'w') as f: yaml.dump(args.__dict__, f, indent=4)
